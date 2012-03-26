692c78dbc63ccd6405dbb982f1312294abe40b88
Javier <javiermon@gmail.com>
2012-03-26 23:39:36 +0200

    add url for tariffs

--- tariffs.py
+++ tariffs.py
@@ -5,8 +5,8 @@
 # http://sam.zoy.org/wtfpl/
 
 tariffs = {
-    'elefantito' : {'minutes' : 0.027, 'data': 6.9, 'establishment': 0.15 },
-    'lobo'       : {'minutes' : 0.022, 'data': 8.5, 'establishment': 0.15 },
-    'tu'         : {'minutes' : 0.030, 'data': 6,   'establishment': 0.15 },
-    'movilona'   : {'minutes' : 0.084, 'data': 6.9, 'establishment': 0 }
+    'elefantito' : {'minutes' : 0.027, 'data': 6.9, 'establishment': 0.15 ,'url': 'http://www.pepephone.com/'},
+    'lobo'       : {'minutes' : 0.022, 'data': 8.5, 'establishment': 0.15  'url': 'http://www.pepephone.com/promo/htcmania/lobo/' },
+    'tu'         : {'minutes' : 0.030, 'data': 6,   'establishment': 0.15, 'url': 'http://www.tuenti.com/#m=Tustore&func=index' },
+    'movilona'   : {'minutes' : 0.084, 'data': 6.9, 'establishment': 0,    'url': 'http://www.pepephone.com/tarifamovilonia9/' }
     }
