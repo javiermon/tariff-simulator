6671b69273752c9b388e57fafa71555c80b32be5
Javier <javiermon@gmail.com>
2012-04-02 18:16:53 +0200

    rename to better fix promo name

--- tariffs.py
+++ tariffs.py
@@ -8,5 +8,5 @@ tariffs = {
     'elefante'   : {'minutes' : 0.027, 'data': 6.9, 'establishment': 0.15, 'url': 'http://www.pepephone.com/'},
     'lobo'       : {'minutes' : 0.022, 'data': 8.5, 'establishment': 0.15, 'url': 'http://www.pepephone.com/promo/htcmania/lobo/' },
     'tu'         : {'minutes' : 0.030, 'data': 6,   'establishment': 0.15, 'url': 'http://www.tuenti.com/#m=Tustore&func=index' },
-    'movilona'   : {'minutes' : 0.084, 'data': 6.9, 'establishment': 0,    'url': 'http://www.pepephone.com/tarifamovilonia9/' }
+    'movilonia9'   : {'minutes' : 0.084, 'data': 6.9, 'establishment': 0,    'url': 'http://www.pepephone.com/tarifamovilonia9/' }
     }
